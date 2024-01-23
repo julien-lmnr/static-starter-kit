2df4566f3cb01bdee1c971a0c8cbeea10e98f244
labfordev <labfordev@gmail.com>
2024-01-23 10:39:28 +0100

    add prettier

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,12 @@
-import { Application } from "@hotwired/stimulus";
+import { Application } from '@hotwired/stimulus'
 // Stimulus controllers
-import Hello from './controllers/hello_controller';
+import Hello from './controllers/hello_controller'
 
 // SCSS
-import './../scss/app.scss';
+import './../scss/app.scss'
 
 // Start Stimulus
-const application = Application.start();
+const application = Application.start()
 
 // Declaration Stimulus controllers
-application.register('hello', Hello);
+application.register('hello', Hello)
